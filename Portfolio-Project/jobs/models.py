932bdac8705284943c61c4d3e778ee235c024b04
Tristan Izlar <59484088+tizlar@users.noreply.github.com>
2022-02-14 18:11:01 -0500

    implementing a detail view for jobs

--- Portfolio-Project/jobs/models.py
+++ Portfolio-Project/jobs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 class Job(models.Model):
+    title = models.CharField(max_length=100, default=None)
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
